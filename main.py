1d68a65f761940a4d78e06e08bb6b4e5ea52cc8c
Phieldar <gabriel.monet@gmail.com>
2020-05-30 10:46:54 -0400

    Personnage

--- main.py
+++ main.py
@@ -35,8 +35,9 @@ class Cite:
 
 class Player:
 
-    def __init__(self, name, health, wealth, influence, virtues, vices, traits):
+    def __init__(self, name, age, health, wealth, influence, virtues, vices, traits):
         self.name = name
+        self.age = age
         self.health = health
         self.wealth = wealth
         self.influence = influence
@@ -47,6 +48,9 @@ class Player:
 
     def get_name(self):
         return self.name
+
+    def get_age(self):
+        return self.age
 
     def get_health(self):
         return self.health
@@ -69,8 +73,6 @@ class Player:
             print("4) explorer la cite")
             print("5) Quitter le jeu")
             choix = int(input("Quel est votre choix?"))
-            if choix is not int:
-                print (str(self.name) + " doit y avoir un choix.")
             if choix == 1:
                 print ("Le Consul lit. Il augmente son influence. Celle-ci était de :" + str(self.influence))
                 self.augmenter_influence(2)
@@ -84,10 +86,25 @@ class Player:
                 cite_joueur.description_cite()
                 decision_joueur = False
             if choix == 5:
+                self.health = 0
                 decision_joueur = False
             else:
-                decision_joueur = False
+                print (str(self.name) + " doit faire un choix.")
 
+    def background_player(self):
+        background_joueur = True
+        while background_joueur == True:
+            print("##### Le passé de " + str(self.name) + " #####")
+            self.get_age() = 18
+            print("Dans sa jeunesse " + str(self.name) + "vivait calmement avec sa famille dans " + str(cite_joueur.name))
+            ## ajouter un choix avec pour résultat l'anour d'un vice et d'une vertu dans le perso
+            print("Cette grande cité était néanmoins gouverné d'une main de fer par un gouvernement autoritaire et despotique.")
+            print("Dans sa jeunesse " + str(self.name) + " a rencontré biens des gens, mais une rencontre a été importante.")
+            print("Il a finalement élu à la tête de la cité à cause de : ")
+            background_choice = int(input("Choix :"))
+            if background_choice == 1:
+                print("Ok")
+            background_joueur = False
         return self
 
 
@@ -99,6 +116,19 @@ class Factions:
         self.richesse = richesse
         self.force = force
         self.cohesion = cohesion
+
+
+class Personnages:
+
+    def __init__(self, name, age, health, wealth, influence, virtues, vices, traits):
+        self.name = name
+        self.age = age
+        self.health = health
+        self.wealth = wealth
+        self.influence = influence
+        self.virtues = virtues
+        self.vices = vices
+        self.traits = traits
 
 
 # Création de la cité
@@ -124,7 +154,7 @@ cite_joueur = Cite(nom_cite, population_cite, factions_cite, richesse_cite, ordr
 richesse_joueur = random.randint(1,5)
 
 joueur_nom = str(input("Quel est le nom du nouveau consul élu dans la cité de " + str(cite_joueur.get_name()) + "!"))
-player1 = Player(joueur_nom, 10, richesse_joueur, 5, ["Courage", "Humilité"], ["Avarice"], ["Grand"])
+player1 = Player(joueur_nom, 10, 10, richesse_joueur, 5, ["Courage", "Humilité"], ["Avarice"], ["Grand"])
 
 #### Structure du jeu #####
 
@@ -133,9 +163,14 @@ tour = 1
 
 while fin_du_jeu == False:
     print("##### " + str(tour) + " #####")
-    player1.decision_player()
-    #Player.decision_player(player1)
-    print("Fin!")
+    if tour == 1:
+        player1.background_player()
+    player1.decision_player()     #Pourquoi pas : Player.decision_player(player1)
+    print("Fin du tour!")
     tour += 1
+    if player1.health == 0:
+        print("Fin du jeu.")
+        fin_du_jeu = True
     if tour == 5:
+        print("Fin du jeu.")
         fin_du_jeu = True
